37679aa7db92f695702a35d26ef0e1c6609d1e47
Logards <deamezagabastien@outlook.fr>
2024-08-14 23:29:36 +0200

    update

--- basket/urls.py
+++ basket/urls.py
@@ -9,5 +9,7 @@ urlpatterns = [
     path('register_post/', views.register_post, name='register_post'),
     path('players/', views.all_players, name='players'),
     path('player/<int:player_id>/', views.player_detail, name='player_detail'),
-    path ('team_detail/<int:team_id>/', views.team_detail, name='team_detail')
+    path('team_detail/<int:team_id>/', views.team_detail, name='team_detail'),
+    path('teams/', views.all_teams, name='teams'),
+    path('matchs/', views.all_matchs, name='matchs')
 ]
--- test.py
+++ test.py
@@ -1,26 +1,13 @@
 import requests
 
 
-def all_players():
+def all_matches():
     headers = {'Authorization': "edf0fdb8-c408-48cb-b376-968c9bf9c9d5"}
-    response = requests.get("https://api.balldontlie.io/v1/players", headers=headers)
-    players = response.json()
-    for player in players['data']:
-        print(player['first_name'], player['last_name'])
+    response = requests.get("https://api.balldontlie.io/v1/games", headers=headers)
+    matches = response.json()
+    print(matches)
 
 
-def all_teams():
-    headers = {'Authorization': "edf0fdb8-c408-48cb-b376-968c9bf9c9d5"}
-    response = requests.get("https://api.balldontlie.io/v1/teams", headers=headers)
-    teams = response.json()
-    for team in teams['data']:
-        if team["conference"] == '    ':
-            team["conference"] = "No information about conference"
-        if team["division"] == "":
-            team["division"] = "No information about division"
-        if team["city"] == "":
-            team["city"] = "No information about city"
-    return teams
+all_matches()
 
 
-print(all_teams())
